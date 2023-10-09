a2e8434537ad9659129acb0309c26278408d8bc5
My Amr <myre.amran@gmail.com>
2023-10-09 09:43:55 +0200

    Intial commit

---
+++ header.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="<?php echo get_bloginfo('language'); ?>">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Photographe d'event">
+
+
+        <?php wp_head();?>
+    </head>
+    <header>
+
+    </header>
+<body>
+    
+
+    
